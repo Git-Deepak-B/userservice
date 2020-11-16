e30e3feaafce6daab7f17c84b5bc2631e2ba8efe
deepak <deepak@dafidy.com>
2020-11-16 23:41:03 +0530

    Added user and role domain for role based user authentication

---
+++ src/main/java/com/test/userservice/model/Role.java
@@ -0,0 +1,26 @@
+package com.test.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(name = "name", unique = true)
+    @NotNull
+    private String name;
+}
---
+++ src/main/java/com/test/userservice/model/User.java
@@ -0,0 +1,53 @@
+package com.test.userservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username", unique = true)
+    @NotNull
+    private String username;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    @Email
+    @NotNull
+    private String email;
+
+    @JsonIgnore
+    @Column(name = "password")
+    @NotNull
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @Column(name = "enabled")
+    private boolean enabled;
+}
