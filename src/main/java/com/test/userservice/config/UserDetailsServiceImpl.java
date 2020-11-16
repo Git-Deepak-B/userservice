93306042e433f9ff24a3be4b2085d19b77805fde
deepak <deepak@dafidy.com>
2020-11-16 23:42:22 +0530

    added spring security jwt configuration

---
+++ src/main/java/com/test/userservice/config/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.test.userservice.config;
+
+import com.test.userservice.model.User;
+import com.test.userservice.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found with given username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
